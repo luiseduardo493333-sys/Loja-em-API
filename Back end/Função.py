28b2536a33a547ccfbd4e6449743c96ddc37f26a
Luis <Luiseduardo493333@gmail.com>
2025-10-20 14:15:20 -0300

    Conexão feita e função adicionar produto

---
+++ Back end/Função.py
@@ -0,0 +1,27 @@
+from conexao import conectar
+def adicionar_produto(nome, categoria, preco, quantidade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "INSERT INTO produtos (nome, categoria, preco, quantidade) VALUES (%s, %s, %s, %s)",
+                (nome, categoria, preco, quantidade)
+            )
+            conexao.commit()
+        finally:
+            cursor.close()
+            conexao.close()
+
+
+def adicionar_produto(nome, categoria, preco, quantidade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "INSERT INTO produtos (nome, categoria, preco, quantidade) VALUES (%s, %s, %s, %s)",
+                (nome, categoria, preco, quantidade)
+            )
+            conexao.commit()
+        finally:
+            cursor.close()
+            conexao.close()
