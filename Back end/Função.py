e23502ec15b2551091168ab02413652c779461b1
Luis <Luiseduardo493333@gmail.com>
2025-10-20 14:17:16 -0300

    função atualizar produto

--- Back end/Função.py
+++ Back end/Função.py
@@ -1,17 +1,21 @@
 from conexao import conectar
-def adicionar_produto(nome, categoria, preco, quantidade):
+def criar_tabela():
     conexao, cursor = conectar()
     if conexao:
         try:
-            cursor.execute(
-                "INSERT INTO produtos (nome, categoria, preco, quantidade) VALUES (%s, %s, %s, %s)",
-                (nome, categoria, preco, quantidade)
-            )
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS produtos (
+                    id SERIAL PRIMARY KEY,
+                    nome VARCHAR(100) NOT NULL,
+                    categoria VARCHAR(50),
+                    preco NUMERIC(10,2),
+                    quantidade INT
+                )
+            """)
             conexao.commit()
         finally:
             cursor.close()
             conexao.close()
-
 
 def adicionar_produto(nome, categoria, preco, quantidade):
     conexao, cursor = conectar()
@@ -34,6 +38,19 @@ def listar_produtos():
         try:
             cursor.execute("SELECT * FROM produtos ORDER BY id")
             return cursor.fetchall()
+        finally:
+            cursor.close()
+            conexao.close()
+
+def atualizar_produto(id_produto, preco=None, quantidade=None):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            if preco is not None:
+                cursor.execute("UPDATE produtos SET preco = %s WHERE id = %s", (preco, id_produto))
+            if quantidade is not None:
+                cursor.execute("UPDATE produtos SET quantidade = %s WHERE id = %s", (quantidade, id_produto))
+            conexao.commit()
         finally:
             cursor.close()
             conexao.close()
